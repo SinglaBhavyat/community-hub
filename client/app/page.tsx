d8aef4eb4d5a1e0442d74db1233332befb41d01f
Bhavyat Singla <bhavyatsingla@Bhavyats-Laptop.local>
2026-01-08 02:23:02 +0530

    Fix: move posts fetch to client to avoid build-time env dependency

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import PostCard from "../components/PostCard";
 import { apiFetch } from "../lib/api";
 
+export default function Home() {
+  const [posts, setPosts] = useState<any[]>([]);
 
-export default async function Home() {
-  const posts = await apiFetch("/api/posts").then(r => r.json());
-  return posts.map((p: any) => <PostCard key={p._id} post={p} />);
+  useEffect(() => {
+    apiFetch("/api/posts")
+      .then(r => r.json())
+      .then(setPosts)
+      .catch(console.error);
+  }, []);
+
+  return (
+    <div>
+      {posts.map(p => (
+        <PostCard key={p._id} post={p} />
+      ))}
+    </div>
+  );
 }
+
